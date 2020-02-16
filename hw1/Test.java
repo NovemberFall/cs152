0e56c77327d37b6204d0145b5a83af3234dc3625
Git <1219.zheng@gmail.com>
2020-02-15 23:18:40 -0800

    update

---
+++ hw1/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(999999999999999999999 * 2);
+    }
+}
---
+++ hw1/big-num.rkt
@@ -0,0 +1,61 @@
+#lang racket
+
+;; The big-num data structure is essentially a list of 3 digit numbers.
+
+;; Exporting methods
+(provide big-add big-subtract big-multiply big-power-of pretty-print
+         number->bignum string->bignum bignum? zero-or-one? one-block?)
+
+(define MAX_BLOCK 1000)
+
+;; Contract verifying the datatype of a bignum
+(define (bignum? n)
+  (cond [(not (list? n)) #f]
+        [(not (list-of-ints? n)) #f]
+        [else #t]))
+
+;; Helper contract
+(define (list-of-ints? lst)
+  (cond [(empty? lst) #t]
+        [(integer? (car lst)) (list-of-ints? (cdr lst))]
+        [else #f]))
+
+;; Contract to ensure a number is 0 or 1.
+(define (zero-or-one? n)
+  (match n
+    [0 #t]
+    [1 #t]
+    [_ #f]))
+
+
+;; Contract to insure a number is an integer in the range of 0-999.
+(define (one-block? n)
+  (and (integer? n)
+       (>= n 0)
+       (< n 1000)))
+
+
+;; Addition of two big-nums
+(define/contract (big-add x y)
+  (-> bignum? bignum? bignum?)
+  (big-add1 x y 0)
+  )
+
+
+(define/contract (big-add1 x y co)
+  (-> bignum? bignum? zero-or-one? bignum?)
+  (cond
+    ;; If both lists are empty, the return value is either 0 or the caryover value.
+    [(and (= 0 (length x)) (= 0 (length y)))
+      (if (= co 0) '() (list co))]
+    [(= 0 (length x))  (big-add1 (list co) y 0)]
+    [(= 0 (length y))  (big-add1 x (list co) 0)]
+    [else
+       ;;
+       ;; --- YOUR CODE HERE ---
+       ;;
+     (error "Not implemented")
+     ]
+    ))
+
+
