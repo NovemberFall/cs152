5c4b191fffba327ff54a8b65bae2b6c436198465
Git <1219.zheng@gmail.com>
2020-03-20 23:45:50 -0700

    update

--- day13-Scope_JS/bind.js
+++ day13-Scope_JS/bind.js
@@ -18,6 +18,12 @@ var john = {
     }
 };
 
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
 
 /*   bind method
 It is very similar to the call method as well, so it also allows us to set the this variable explicitly.
@@ -29,3 +35,32 @@ var johnFriendly = john.presentation.bind(john, 'friendly');
 johnFriendly('morning');
 johnFriendly('night');
 
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+
+
+/* function as arguments; bind method */
+var years = [1990, 1965, 1937, 2005, 1998];
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2019 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
